7e0649b400c4c9f51abfb525cdc607c6065c6b31
Dw37 <kpdwyer@live.com>
2017-03-20 19:44:51 -0400

    checker BG

--- SpriteMaker/Assets/SpriteMaker/Editor/SpritePreviewEditor.cs
+++ SpriteMaker/Assets/SpriteMaker/Editor/SpritePreviewEditor.cs
@@ -1,30 +1,56 @@
 ﻿using UnityEngine;
 using UnityEditor;
-namespace SpriteMaker{
 
-	/// <summary>
-	/// Handles the SPrite Preview Window
-	/// </summary>
-	public class SpritePreviewEditor : EditorWindow {
+namespace SpriteMaker
+{
 
-		public Texture2D mTex;
-		private Texture2D mGray;
+    /// <summary>
+    /// Handles the SPrite Preview Window
+    /// </summary>
+    public class SpritePreviewEditor : EditorWindow
+    {
+
+        public Texture2D mTex;
+        private Texture2D mGray;
+        private Material eMaterial;
+        private Material aMaterial;
+
+        private const float scaleFactor = 16.0f;
+        //probably best not to go past 24
 
 
-		void OnGUI()
-		{
-			if (mTex != null) {
-				EditorGUI.DrawPreviewTexture (new Rect(0,0,position.width,position.height),mTex,null,ScaleMode.ScaleToFit,1.0f);
-			}
-		}
 
-		public void SetTexture(Texture2D _tex)
-		{
+        void OnGUI()
+        {
+            if (mGray == null)
+            {
+                mGray = Resources.Load<Texture2D>("Checkers");
+            }
 
-			mTex = _tex;
-			mTex.filterMode = FilterMode.Point;
-			base.Repaint ();
+    
+                    
+            Rect g = new Rect(0, 0, position.width, position.height);
+            EditorGUI.DrawPreviewTexture(g, mGray, null, ScaleMode.StretchToFill, 1.0f);
 
-		}
-	}
+
+            if (eMaterial == null)
+            {
+                eMaterial = Resources.Load<Material>("EditorMaterial");
+            }
+
+            if (mTex != null)
+            {
+                EditorGUI.DrawPreviewTexture(new Rect(0, 0, position.width, position.height), mTex, eMaterial, ScaleMode.ScaleToFit, 1.0f);
+            }
+        }
+
+        public void SetTexture(Texture2D _tex)
+        {
+
+            mTex = _tex;
+            mTex.filterMode = FilterMode.Point;
+            base.Repaint();
+
+        }
+    }
 }
